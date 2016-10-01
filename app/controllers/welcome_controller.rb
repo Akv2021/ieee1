485b1b48e9adbc6118b22ed1dd6d31024dd7a024
Mayank Kashyap <mayankkashyap95@gmail.com>
2016-10-01 11:22:51 +0530

    final

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,25 +1,28 @@
 class WelcomeController < ApplicationController
 
 def index
-@rightwidgets = Rightwidget.all
-@blogs = Blog.all
-@navigations = Navigation.all
-@events = Event.all
+	@rightwidgets = Rightwidget.all
+	@blogs = Blog.all
+	@events = Event.all
 end
 
 def admin
 end
 
 def what
+	render layout: false
 end
 
 def join
+	render layout: false
 end
 
 def benefits
+	render layout: false
 end
 
 def resource
+	render layout: false
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 #get 'welcome/index'
 get 'welcome/admin'
+get 'welcome/benefits'
+get 'welcome/join'
+get 'welcome/resource'
+get 'welcome/what'
 match ':controller(/:action(/:id))', :via => :get
 
 root "welcome#index"
